4aa63a94ecc55089b63d6eeb744029601b298438
Clay Wintringham <aikenwintringham@outlook.com>
2023-06-20 22:26:31 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const selectionButtons = document.querySelectorAll("[data-selection]");
+const finalColumn = document.querySelector("[data-final-column]");
+// const anyColumn = document.querySelector("[data-any-column]"); //test line
+
+const computerScoreSpan = document.querySelector("[data-computer-score]");
+const yourScoreSpan = document.querySelector("[data-your-score]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "✊",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    emoji: "✋",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    emoji: "✌",
+    beats: "paper",
+  },
+];
+
+selectionButtons.forEach((selectionButton) => {
+  selectionButton.addEventListener("click", (e) => {
+    const selectionName = selectionButton.dataset.selection;
+    const selection = SELECTIONS.find(
+      (selection) => selection.name === selectionName //selection is selected emoji, i.e. your turn
+    );
+    makeSelection(selection);
+  });
+});
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection();
+  const yourWinner = isWinner(selection, computerSelection);
+  const computerWinner = isWinner(computerSelection, selection);
+
+  addSelectionResult(computerSelection, computerWinner);
+  addSelectionResult(selection, yourWinner);
+
+  if (yourWinner) incrementScore(yourScoreSpan);
+
+  if (computerWinner) incrementScore(computerScoreSpan);
+}
+
+function incrementScore(scoreSpan) {
+  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+}
+
+function addSelectionResult(selection, winner) {
+  const div = document.createElement("div");
+  div.innerText = selection.emoji; //selection is clicked emoji
+  div.classList.add("result-selection");
+  if (winner) div.classList.add("winner"); //STUDY!
+  finalColumn.after(div);
+}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name; //true then the opponent is beaten
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS[randomIndex];
+}
